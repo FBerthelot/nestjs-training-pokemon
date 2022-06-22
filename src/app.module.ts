90f71a32555e7ee8c72d7dd01883ddcb559ba523
FBerthelot <florent.berthelot35@gmail.com>
2022-06-22 23:27:58 +0200

    exo 12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PokemonController } from './pokemon/pokemon.controller';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { PokemonService } from './pokemon/pokemon.service';
 import { HttpModule } from '@nestjs/axios';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule],
-  controllers: [AppController, PokemonController, AuthController],
-  providers: [AppService, AuthService, PokemonService],
+  imports: [HttpModule, PokemonModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
