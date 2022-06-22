dd7bdbf769723617d969cb7972b8fbcdf1fdc09e
FBerthelot <florent.berthelot35@gmail.com>
2022-06-22 23:48:05 +0200

    exo 15

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LogInterceptor } from './log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalInterceptors(new LogInterceptor());
 
   await app.listen(3000);
 }
