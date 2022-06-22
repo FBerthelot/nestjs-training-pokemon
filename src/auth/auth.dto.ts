4ec7703319f992495fc64e939e6fc275517429b1
FBerthelot <florent.berthelot35@gmail.com>
2022-06-22 23:30:47 +0200

    exo 13

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, MinLength } from 'class-validator';
+
 export class AuthBodyDTO {
+  @IsString()
+  @MinLength(1)
   name: string;
+
+  @IsString()
+  @MinLength(1)
   password: string;
 }
--- src/pokemon/pokemon.dto.ts
+++ src/pokemon/pokemon.dto.ts
@@ -1,9 +1,24 @@
+import { IsNumber, IsString, Min, min, MinLength } from 'class-validator';
 import { Pokemon } from './pokemon.model';
 
 export class CreatePokemonDTO implements Pokemon {
+  @IsString()
+  @MinLength(1)
   id: string;
+
+  @IsString()
+  @MinLength(1)
   name: string;
+
+  @IsNumber()
+  @Min(1)
   speed: number;
+
+  @IsNumber()
+  @Min(1)
   attack: number;
+
+  @IsNumber()
+  @Min(50)
   hp: number;
 }
